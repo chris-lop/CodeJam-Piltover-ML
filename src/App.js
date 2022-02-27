a081619ef1f05a9851f44171b980d9e4c947d737
Chris Lopez <73512428+chris-lop@users.noreply.github.com>
2022-02-26 22:49:29 -0500

    Carousel App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Timer from './Timer';
 import './App.css';
 import Timer90Minutes from './Timer90Minutes';
 import Timer5217rule from './Timer5217rule';
+import Carousel from 'react-bootstrap/Carousel';
+import { Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   const [isToggled, setIsToggled] = useState(false);
@@ -12,6 +14,20 @@ function App() {
       { isToggled && <Timer />}
       { isToggled && <Timer90Minutes />}
       { isToggled && <Timer5217rule />}
+
+      <Container>
+        <Carousel className="Carousel" interval="null">
+          <Carousel.Item>
+            <Timer />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Timer90Minutes />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Timer5217rule />
+          </Carousel.Item>
+        </Carousel>
+      </Container>
     </div>
   )
 }
